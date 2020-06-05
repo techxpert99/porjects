522f5dff58d72cd252724963905bb62166b081b8
Ritik Jain <rjain2@cs.iitr.ac.in>
2020-06-05 15:53:48 +0530

    5

--- autopush.py
+++ autopush.py
@@ -1,5 +1,6 @@
 import subprocess
 out = subprocess.Popen(['git','status'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-stdout,stderr = out.communicate()
-if str(out).lower().__contains__('nothing to commit'):
+stdout,_ = out.communicate()
+stdout = str(stdout).lower()
+if 'nothing to commit' in stdout:
     print('Meow')
