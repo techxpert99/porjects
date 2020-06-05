a8084459f937c1afdc39887898fb0a1828e8c1fe
Ritik Jain <rjain2@cs.iitr.ac.in>
2020-06-05 19:50:41 +0530

    autopush:96

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-$(document).ready(function(){
-
-$('#name').on("input",function(){
-    var val = $('#name').val()
-    var num = 0
-    for(var index=val.length-1; index>=0; index--)
-        num += val.charCodeAt(index);
-    $('#num').html(num);
-})
-
-});
+()();
