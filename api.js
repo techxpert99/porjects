220a684bb336c21906466ab3dc43e9f689f95b94
Ritik Jain <rjain2@cs.iitr.ac.in>
2020-06-05 19:11:20 +0530

    autopush:78

--- api.js
+++ api.js
@@ -4,11 +4,9 @@ $('#name').on("input",function(){
     var val = $('#name').val()
     var num = 0
     var pow = 1
-    for(index=len(val)-1; index>=0; index--){
-        num += pow*number(val[index])
-    }
-
-    $('#num').html($('#name').val());
+    for(index=len(val)-1; index>=0; index--)
+        num += pow*number(val[index]);
+    $('#num').html(num);
 })
 
 });
