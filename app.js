5813aeae8f8843f2fb8d25471bb18942ee127627
Ritik Jain <rjain2@cs.iitr.ac.in>
2020-06-08 22:32:08 +0530

    autopush:142

--- app.js
+++ app.js
@@ -6,5 +6,11 @@
     .controller('NameCalculatorController', ['$scope', function($scope) {
         $scope.name = "";
         $scope.totalValue = 0;
+        $scope.updateNumeric = (function() {
+            var totalNameValue = 0;
+            var name = $scope.name
+            for (var index = 0; index < name.length; index++)
+                totalNameValue += name.charCodeAt(index)
+        });
     }]);
 })();
