9474a63fc15ccff8ddb1ec3a101a0671032c9201
Ritik Jain <rjain2@cs.iitr.ac.in>
2020-06-08 23:37:33 +0530

    autopush:169

---
+++ project-1/scripts/app.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+
+    angular.module('DIApp', [])
+
+    .controller('DIController', ['$scope', '$filter', function($scope, $filter) {
+        $scope.name = "";
+        $scope.upper = function() {
+            var upcase = $filter('uppercase');
+            $scope.name = upcase($scope.name);
+        };
+    }]);
+})();
