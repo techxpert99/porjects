aad55ed1006e0a9f1559e94b0faaafa1aa5e1142
Ritik Jain <rjain2@cs.iitr.ac.in>
2020-06-08 23:54:30 +0530

    autopush:175

--- project-1/scripts/app.js
+++ project-1/scripts/app.js
@@ -6,7 +6,7 @@
     .controller('LCCtl', ['$scope', function($scope) {
         $scope.message = "";
         $scope.lunch_menu = "";
-        $scope.checkMenu() = function() {
+        $scope.checkLunch() = function() {
             $lunch = $scope.lunch_menu;
             if ($lunch == 0 || $lunch.length == 0)
                 $cope.message = "Please enter data first";
