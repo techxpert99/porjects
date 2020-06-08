0959ab689c857b4792c408479aecaa15cdb177db
Ritik Jain <rjain2@cs.iitr.ac.in>
2020-06-08 23:59:15 +0530

    autopush:183

--- project-1/scripts/app.js
+++ project-1/scripts/app.js
@@ -6,7 +6,7 @@
     .controller('LCCtl', ['$scope', function($scope) {
         $scope.message = "";
         $scope.lunch_menu = "";
-        $scope.background = "#0000FF";
+        $scope.color = "#FF00FF";
         $scope.checkLunch = function() {
             var lunch = $scope.lunch_menu;
             if (lunch == 0 || lunch.length == 0)
