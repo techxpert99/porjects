acdb7b95b879d7759a8aebe0a60a29a451952a35
Ritik Jain <rjain2@cs.iitr.ac.in>
2020-06-08 22:19:42 +0530

    autopush:129

--- autopush.py
+++ autopush.py
@@ -2,7 +2,7 @@ from subprocess import Popen as popen,PIPE,STDOUT
 from time import sleep
 from re import split
 SAVE_FILE = "autopush.py"
-COMMIT_NUMBER = int("0000128")
+COMMIT_NUMBER = int("0000129")
 RUNNABLE = True
 SLEEP_TIME = 5
 
