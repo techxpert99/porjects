f774bc9b9e4dad738c0b5a4e6f54c300756b3048
Ritik Jain <rjain2@cs.iitr.ac.in>
2020-06-10 17:08:07 +0530

    autopush:239

--- project-1/scripts/app.js
+++ project-1/scripts/app.js
@@ -1,30 +1,36 @@
 (function() {
     'use strict';
 
+    var shoppingList1 = [
+        "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter"
+    ];
+
+    var shoppingList2 = [{
+            name: "Milk",
+            quantity: "2"
+        },
+        {
+            name: "Donuts",
+            quantity: "200"
+        },
+        {
+            name: "Cookies",
+            quantity: "300"
+        },
+        {
+            name: "Chocolate",
+            quantity: "5"
+        }
+    ];
+
     angular.module('SampleApp', [])
+        .controller('SampleCtl', SampleController);
 
-    .controller('SampleCtl', ['$scope', function($scope) {
+    SampleController.$inject = ['$scope'];
 
-        var shoppingList1 = [
-            "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter"
-        ];
+    function SampleController($scope) {
+        $scope.shoppingList1 = shoppingList1;
+        $scope.shoppingList2 = shoppingList2;
+    }
 
-        var shoppingList2 = [{
-                name: "Milk",
-                quantity: "2"
-            },
-            {
-                name: "Donuts",
-                quantity: "200"
-            },
-            {
-                name: "Cookies",
-                quantity: "300"
-            },
-            {
-                name: "Chocolate",
-                quantity: "5"
-            }
-        ];
-    }]);
 })();
