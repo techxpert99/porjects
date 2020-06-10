ba741a489423869acdaef09dfe1d6b808de3b083
Ritik Jain <rjain2@cs.iitr.ac.in>
2020-06-10 17:28:55 +0530

    autopush:255

--- autopush.py
+++ autopush.py
@@ -2,7 +2,7 @@ from subprocess import Popen as popen,PIPE,STDOUT
 from time import sleep
 from re import split
 SAVE_FILE = "autopush.py"
-COMMIT_NUMBER = int("0000254")
+COMMIT_NUMBER = int("0000255")
 RUNNABLE = True
 SLEEP_TIME = 5
 
--- filter.js
+++ filter.js
@@ -1,3 +1,9 @@
 var numArray = [];
 for (i = 1; i <= 10; i++) numArray.push(i);
 window.console && console.log("Number array: ", numArray);
+
+var filteredNumArray = numArray.filter(function(value) {
+    return value > 5;
+});
+
+window.console && console.log("Filtered Array: ", filteredNumArray);
