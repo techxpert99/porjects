474441ecc94c946b03169fa118f94e26d6a1b30f
Ritik Jain <rjain2@cs.iitr.ac.in>
2020-06-10 17:06:27 +0530

    autopush:236

--- project-1/scripts/app.js
+++ project-1/scripts/app.js
@@ -5,6 +5,26 @@
 
     .controller('SampleCtl', ['$scope', function($scope) {
 
+        var shoppingList1 = [
+            "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter"
+        ];
 
+        var shoppingList2 = [{
+                name: "Milk",
+                quantity: "2"
+            },
+            {
+                name: "Donuts",
+                quantity: "200"
+            },
+            {
+                name: "Cookies",
+                quantity: "300"
+            },
+            {
+                name: "Chocolate",
+                quantity: "5"
+            }
+        ];
     }]);
 })();
