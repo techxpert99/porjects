ecba71c627b83b44b5b2e47677b455fd284704b1
Ritik Jain <rjain2@cs.iitr.ac.in>
2020-06-10 17:01:56 +0530

    autopush:228

--- project-1/scripts/app.js
+++ project-1/scripts/app.js
@@ -1,31 +1,10 @@
 (function() {
     'use strict';
 
-    angular.module('LunchChecker', [])
+    angular.module('SampleApp', [])
 
-    .controller('LCCtl', ['$scope', function($scope) {
-        $scope.message = "";
-        $scope.lunch_menu = "";
-        $scope.color = "black";
-        $scope.checkLunch = function() {
-            var lunch = $scope.lunch_menu;
-            if (lunch == 0 || lunch.length == 0) {
-                $scope.color = "red";
-                $scope.message = "Please enter data first";
-            } else {
-                $scope.color = "green";
-                lunch = lunch.split(',');
-                var count = 0;
-                lunch.forEach(function(item) {
-                    console.log("*" + item);
-                    if (item != 0 && item.trim() != 0 && item.trim().length != 0)
-                        count += 1;
-                });
-                if (count <= 3)
-                    $scope.message = "Enjoy!";
-                else
-                    $scope.message = "Too Much!";
-            }
-        };
+    .controller('SampleCtl', ['$scope', function($scope) {
+
+
     }]);
 })();
