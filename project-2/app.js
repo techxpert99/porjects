81cce7cd60af448204782baf2361bc1480d03c98
techxpert99 <54289562+techxpert99@users.noreply.github.com>
2020-06-11 22:14:34 +0530

    Update app.js

--- project-2/app.js
+++ project-2/app.js
@@ -1,4 +1,4 @@
-(function() {
+/*(function() {
     angular.module('app', [])
         .controller('appctl', AppCtl);
 
@@ -18,5 +18,81 @@
         $scope.buyItem = function(index) {
             $scope.boughtList.push($scope.shoppingList.splice(index, 1)[0]);
         };
+    }
+})();*/
+
+(function(){
+
+    'use strict';
+    angular.module('ShoppingListCheckOff',[])
+    .controller("ToBuyController",ToBuyController)
+    .controller("AlreadyBoughtController",AlreadyBoughtController)
+    .service("ShoppingListCheckOffService",ShoppingListCheckOffService);
+
+    ToBuyController.$inject=["ShoppingListCheckOffService"];
+
+    function ToBuyController (ShoppingListCheckOffService){
+        var buy=this;
+        
+        buy.buy_items=ShoppingListCheckOffService.getBuy_items();
+
+        buy.checkOff=function(index){
+
+            ShoppingListCheckOffService.checkOff(index);
+        }
+    }
+    AlreadyBoughtController.$inject=["ShoppingListCheckOffService"];
+
+    function AlreadyBoughtController (ShoppingListCheckOffService){
+        var bought=this;
+        
+        bought.b_items=ShoppingListCheckOffService.getBought_items();
+
+    }
+    function ShoppingListCheckOffService(){
+        var service=this;
+        var buy_items=[
+            {
+                name:'Chocolates',
+                quantity:'10 bags'
+            },
+            {
+                name:'Ice-Cream',
+                quantity:'10 packets'
+            
+            },
+            {
+                name:'Cheese',
+                quantity:'5 cubes'
+            
+            },
+            {
+                name:'Oil',
+                quantity:'1 Bottle'
+            
+            },
+            {
+                name:'Soap',
+                quantity:'3 boxes'
+            
+            },
+
+        ];
+
+        var bought=[];
+        service.getBuy_items=function()
+        {
+            return buy_items;
+        }
+        service.checkOff=function(index){
+            bought.push(buy_items[index]);
+            buy_items.splice(index,1);
+
+            console.log(bought);
+            console.log(buy_items);
+        }
+        service.getBought_items=function(){
+            return bought;
+        }
     }
 })();
