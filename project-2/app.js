fafd7adc5da60ae58b0324d0ec9d8eacf6da0985
techxpert99 <54289562+techxpert99@users.noreply.github.com>
2020-06-11 22:16:25 +0530

    Update app.js

--- project-2/app.js
+++ project-2/app.js
@@ -1,26 +1,3 @@
-/*(function() {
-    angular.module('app', [])
-        .controller('appctl', AppCtl);
-
-    var shoppingList = [
-        { name: 'Cookie', quantity: 10 },
-        { name: 'Chocolate', quantity: 5 },
-        { name: 'Ice Cream', quantity: 7 },
-        { name: 'Dog shit', quantity: 1000 },
-        { name: 'Cat food', quantity: 50 }
-    ];
-
-    AppCtl.$inject = ['$scope'];
-
-    function AppCtl($scope) {
-        $scope.shoppingList = shoppingList;
-        $scope.boughtList = [];
-        $scope.buyItem = function(index) {
-            $scope.boughtList.push($scope.shoppingList.splice(index, 1)[0]);
-        };
-    }
-})();*/
-
 (function(){
 
     'use strict';
